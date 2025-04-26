6d2bde4fe6f1ad826b9d7a4fc23bfa52ed5cb396
Abdullah Hussain <stacksolutionhub@gmail.com>
2025-04-26 01:13:06 -0700

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-response = requests.get("https://dog.ceo/api/breeds/image/random")
-
-print("Status:", response.status_code)
-print("Headers:")
-for k, v in response.headers.items():
-    print(f"{k}: {v}")
